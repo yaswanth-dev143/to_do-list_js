216b7b159b3c6b4cadc26e129aefe2e82f734556
yaswanth <yeswanthdev999@gmail.com>
2023-02-27 21:31:57 +0530

    akhli anna file

--- todolist.js
+++ todolist.js
@@ -26,14 +26,22 @@ let display = () => {
   if (taskList.length !== 0) {
     let eachtask = ``;
     for (let task of taskList) {
-      eachtask += `<div
+      eachtask += `<li
       class="bg-cyan-500 mt-[20px] h-[40px] rounded-xl flex justify-between content-center"
     >
       <p class="mt-2 ml-3 w-[30%] overflow-hidden">${task}</p>
-      <button class="mr-[12px] text-white" id="close">clear</button>
-    </div>`;
+      <button class="mr-[12px] text-white butt" id="close">clear</button>
+    </li>`;
     }
     taskCont.innerHTML = eachtask;
   }
 };
 display();
+
+//remove task
+let taskCont = document.querySelector("#task_cont");
+taskCont.addEventListener("click", (event) => {
+  let targetElement = event.target;
+  if (targetElement.classList.contains("butt")) {
+  }
+});
